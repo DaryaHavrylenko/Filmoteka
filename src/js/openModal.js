307cffce0be9bd524e46a3a72886c008ae82a0fd
Valeriia-Livinska <valerylivi@gmail.com>
2022-10-11 18:10:28 +0300

    over and over again

--- src/js/openModal.js
+++ src/js/openModal.js
@@ -1,6 +1,6 @@
 import addAndRemoveQueue from './add&removeQueue'; // Queue
 import checkPresenceMovieInQueue from './checkPresenceMovieInQueue'; // Queue
-// import onOpenMovieCard from './addToWatchedBtn'; // добавила Лера для Watched btn
+import onOpenMovieCard from './addToWatchedBtn'; // добавила Лера для Watched btn
 
 export default async function findLi() {
   const backdrop = document.querySelector('.modal-backdrop');
@@ -17,8 +17,8 @@ export default async function findLi() {
 
       renderMarkupModal(searchValue);
       addAndRemoveQueue(searchValue); //Queue
-      checkPresenceMovieInQueue(); //Queue
-      // onOpenMovieCard(); // добавила Лера для Watched btn
+      // checkPresenceMovieInQueue(); //Queue
+      onOpenMovieCard(); // добавила Лера для Watched btn
     } catch (error) {
       console.log(error);
     }
