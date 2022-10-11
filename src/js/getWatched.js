4e808b0022aa9786ad3eb34644595996c58dccfe
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-10-11 23:08:57 +0300

    modal watched

--- src/js/getWatched.js
+++ src/js/getWatched.js
@@ -1,9 +1,37 @@
-// const api_key = "301d018a3b09052968e9ce18b1793bab";
+import renderMarkupMovieCard from './markapTempllate';
+import findLi from './openModal';
+import openModal from './openModal';
 
 
-// fetch(`
-// https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`)
-// .then(res => res.json())
-// .then(res => console.log(res))
+const refs = {
+    btnWatched:document.querySelector('.btn-header'),
+    ul:document.querySelector('.gallery'),
+}
+
+refs.btnWatched.addEventListener('click', getWatched);
+
+function getWatched() {
+const fromLS = localStorage.getItem("watched");
 
 
+
+if(fromLS === "[]" || fromLS === null ){
+  clear();
+  return refs.ul.insertAdjacentHTML("afterbegin", "<li><h1 style=color:green> you did not see nothing, you can could chouse new intresting film on main page</h1></li>");
+}
+clear();
+const film = JSON.parse(fromLS)
+renderMarkupMovieCard(film)
+// findLi();
+// openModal();
+console.log("after");
+}
+
+function clear() {
+    refs.ul.innerHTML = " "
+}
+
+
+function modalWatched() {
+  
+}
