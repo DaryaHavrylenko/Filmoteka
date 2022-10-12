e3230b532fea42cdbe05ad1188f3523750eea8a6
Volodymyr Husak <volodyaog2907@gmail.com>
2022-10-12 16:12:13 +0300

    creat function active and check movies for btn watched and queue

---
+++ src/js/deleteMovieWithQueueAtWatched.js
@@ -0,0 +1,28 @@
+import getArrQueueWithLocalStorage from './getArrQueueWithLocalStorage';
+export default function deleteMovieWithQueueAtWatched(idMovie) {
+  //   console.log('Видаляєм з Queue фільм з id: ', idMovie);
+  const arrMovieWithQueue = getArrQueueWithLocalStorage();
+  arrMovieWithQueue?.map((film, index) => {
+    let newArrMovieInQueue = arrMovieWithQueue;
+    if (film.id === idMovie) {
+      //   console.log('Цей фільм є у Queue з id: ', idMovie);
+
+      newArrMovieInQueue.splice(index, 1);
+      setArrQueueInLocalStorage(newArrMovieInQueue); //пушимо новий масив у Queue
+      // Міняє стан кнопки "Add to Queue"
+      let btnAddToQueueEl = document.querySelector('.film-add__queue');
+      btnAddToQueueEl.textContent = 'Add to Queue';
+      btnAddToQueueEl.classList.remove('film-add__queue-active');
+    }
+    return;
+  });
+
+  function setArrQueueInLocalStorage(arr) {
+    try {
+      const serializedState = JSON.stringify(arr);
+      localStorage.setItem('FilmsArrQueue', serializedState);
+    } catch (error) {
+      console.error('Set state error: ', error.message);
+    }
+  }
+}
