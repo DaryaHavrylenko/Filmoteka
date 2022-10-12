76edf3a63dcc1e408f902e921ba5b2bc7bd26aa3
Volodymyr Husak <volodyaog2907@gmail.com>
2022-10-13 00:39:56 +0300

    fix btn

--- src/js/getWatched.js
+++ src/js/getWatched.js
@@ -3,7 +3,8 @@ import findLi from './modalWatched';
 import openModal from './openModal';
 
 const refs = {
-  btnWatched: document.querySelector('.btn-header'),
+  btnWatched: document.querySelector('.btn-header__watched'),
+  btnQueue: document.querySelector('.btn-header__queue'),
   ul: document.querySelector('.gallery'),
 };
 
@@ -11,20 +12,28 @@ refs.btnWatched.addEventListener('click', getWatched);
 
 export default function getWatched() {
   const fromLS = localStorage.getItem('watched');
+  refs.btnQueue.classList.remove('btn-header--active');
+  refs.btnWatched.classList.add('btn-header--active');
 
   if (fromLS === '[]' || fromLS === null) {
     clear();
     // return refs.ul.insertAdjacentHTML(
     //   'afterbegin',
-      const title = '<h1 class="title-queue"> your list is empty </h1>';
-      const container = document.querySelector('.container');
-        const section = document.querySelector('.gallery-section');
-        section.classList.add('library-plug');
-        container.innerHTML = title;
-        return;
+    const title = `<h1 class="title-queue">Your queue is empty</h1>
+    <ul class="gallery gallery--library"></ul>`;
+    const container = document.querySelector('.container');
+    const section = document.querySelector('.gallery-section');
+    section.classList.add('library-plug');
+    container.innerHTML = title;
+    return;
   }
+  const titleEl = document.querySelector('.title-queue');
+  titleEl?.remove();
+  const section = document.querySelector('.gallery-section');
+  section?.classList.remove('library-plug');
   clear();
   const arrayFilms = JSON.parse(fromLS);
+  arrayFilms.reverse();
   renderMarkupMovieCard(arrayFilms);
   findLi();
 }
@@ -32,5 +41,3 @@ export default function getWatched() {
 function clear() {
   refs.ul.innerHTML = ' ';
 }
-
-function modalWatched() {}
--- src/js/renderQueueMarkup.js
+++ src/js/renderQueueMarkup.js
@@ -4,26 +4,37 @@ import openModalLibraryQueue from './openModalLibreryQueue';
 
 export default function renderQueueMarkup() {
   const btnQueue = document.querySelector('.btn-header__queue');
+  const btnWatched = document.querySelector('.btn-header__watched');
 
   btnQueue.addEventListener('click', onQueue);
 
   function onQueue(event) {
     event.preventDefault();
+    btnWatched.classList.remove('btn-header--active');
+    btnQueue.classList.add('btn-header--active');
+
     const galleryEl = document.querySelector('.gallery');
     galleryEl.innerHTML = '';
+    // const galleryEl = document.querySelector('.container');
+    // galleryEl.innerHTML = '';
 
     const parsedFilmsInQueue = getArrQueueWithLocalStorage();
 
     if (!parsedFilmsInQueue || parsedFilmsInQueue.length === 0) {
-
-      const title = '<h2 class="title-queue">Your queue is empty</h2>';
+      // container.innerHTML = '';
+      const title = `<h1 class="title-queue">Your queue is empty</h1>
+    <ul class="gallery gallery--library"></ul>`;
       const container = document.querySelector('.container');
       const section = document.querySelector('.gallery-section');
       section.classList.add('library-plug');
       container.innerHTML = title;
       return;
-
     }
+
+    const titleEl = document.querySelector('.title-queue');
+    titleEl?.remove();
+    const section = document.querySelector('.gallery-section');
+    section?.classList.remove('library-plug');
 
     renderMarkupMovieCard(parsedFilmsInQueue);
     openModalLibraryQueue();
