926dc76a79ec5ddfba5e7c44489dda62fdf14485
Valeriia-Livinska <valerylivi@gmail.com>
2022-10-14 15:05:41 +0300

    pafination fixing on class

--- src/js/fetchMovie.js
+++ src/js/fetchMovie.js
@@ -1,28 +1,21 @@
-// рефакторинг на async await + axios
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import renderMarkupMovieCard from './markapTempllate';
 import findLi from './openModal';
 import delay, { visibleSpinner, hideSpinner } from './delay';
 import axios from 'axios';
 
+import FilmsPagination from './pagination-again';
 import Pagination from 'tui-pagination';
 
-const container = document.getElementById('pagination');
 const gallery = document.querySelector('.gallery');
-
-const options = {
-  totalItems: 0,
-  itemsPerPage: 20,
-  visiblePages: 5,
-  page: 1,
-};
-
-const pagination = new Pagination(container, options);
-const paginPage = pagination.getCurrentPage();
-
 const searchForm = document.querySelector('.search__form');
-
 searchForm.addEventListener('submit', onInput);
+
+const container = document.getElementById('pagination');
+
+let filmsSearch;
+let paginator;
+let movieName;
 
 const url = `https://api.themoviedb.org/3/search/movie?`;
 
@@ -41,21 +34,24 @@ async function onInput(event) {
     //    event.preventDefault();
     //  }
     event.preventDefault();
-    const movieName = getQuery();
+    movieName = getQuery();
     if (!movieName) {
       Notify.failure('Please enter the movie name');
       return;
     }
     searchParams.set('query', movieName);
-
-    const results = await fetchMovie(paginPage);
-
+    const results = await fetchMovie(filmsSearch);
+    console.log(results);
     // ???? записываем занчение текущего поиска в local storage для отслеживания разметки при скролле
     // localStorage.setItem('searchQuery', JSON.stringify(results));
     // if (results.page === 1) {
     //   clearGalleryMarkup();
     // }
     clearGalleryMarkup();
+
+    paginator = new FilmsPagination(filmsSearch, total_results);
+    paginator.pagination.on('afterMove', paginatePage);
+
     renderMarkupMovieCard(results);
     updateLocalStorage(results);
     findLi();
@@ -68,25 +64,23 @@ function getQuery() {
   return searchForm.elements[0].value.trim();
 }
 
-pagination.on('afterMove', updatePagination);
-
-async function updatePagination(event) {
+async function paginatePage(event) {
+  
   const currentPage = event.page;
-  console.log('currentPage внизу', currentPage);
-
-  const results = await fetchMovie(currentPage); //?????????????????
+  filmsSearch = movieName;
   clearGalleryMarkup();
-  renderMarkupMovieCard(results);
-  updateLocalStorage(results);
-  findLi();
+  const responce = await fetchMovie(currentPage);
+  renderMarkupMovieCard(responce);
+  console.log(paginator);
 }
+
+let total_results;
 
 async function fetchMovie(page) {
   visibleSpinner();
   searchParams.set('page', page);
-
   const { data } = await axios.get(`${url}${searchParams}`);
-
+  
   // для красивого спинера
   delay(500).then(() => {
     hideSpinner();
@@ -96,19 +90,134 @@ async function fetchMovie(page) {
     Notify.failure('Search result is not successful. Please, try again');
     searchForm.elements[0].value = '';
   }
-  const { results, total_results } = data;
-  pagination.reset(total_results);
-
+  const { results } = data;
+  total_results = data.total_results;
   return results;
 }
+
 function clearGalleryMarkup() {
   gallery.innerHTML = '';
 }
+
 function updateLocalStorage(results) {
   localStorage.setItem('currentPopularMovies', JSON.stringify(results));
 }
-// -----------------------------------------------------------------------------------
 
+// --------------------------------------------------------------------------------------
+// внизу версия которая работала до пагинаци на классе
+// --------------------------------------------------------------------------------------
+
+// рефакторинг на async await + axios
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import renderMarkupMovieCard from './markapTempllate';
+// import findLi from './openModal';
+// import delay, { visibleSpinner, hideSpinner } from './delay';
+// import axios from 'axios';
+
+// import Pagination from 'tui-pagination';
+
+// const container = document.getElementById('pagination');
+// const gallery = document.querySelector('.gallery');
+
+// const options = {
+//   totalItems: 0,
+//   itemsPerPage: 20,
+//   visiblePages: 5,
+//   page: 1,
+// };
+
+// const pagination = new Pagination(container, options);
+// const paginPage = pagination.getCurrentPage();
+
+// const searchForm = document.querySelector('.search__form');
+
+// searchForm.addEventListener('submit', onInput);
+
+// const url = `https://api.themoviedb.org/3/search/movie?`;
+
+// const searchParams = new URLSearchParams({
+//   api_key: '301d018a3b09052968e9ce18b1793bab',
+//   language: 'en-US',
+//   page: 1,
+//   include_adult: false,
+//   query: '',
+// });
+
+// async function onInput(event) {
+//   try {
+//     // отменяем превент дефолт только при первом поиске при скролле
+//     //  if (searchParams.get('page') === 1) {
+//     //    event.preventDefault();
+//     //  }
+//     event.preventDefault();
+//     const movieName = getQuery();
+//     if (!movieName) {
+//       Notify.failure('Please enter the movie name');
+//       return;
+//     }
+//     searchParams.set('query', movieName);
+
+//     const results = await fetchMovie(paginPage);
+
+//     // ???? записываем занчение текущего поиска в local storage для отслеживания разметки при скролле
+//     // localStorage.setItem('searchQuery', JSON.stringify(results));
+//     // if (results.page === 1) {
+//     //   clearGalleryMarkup();
+//     // }
+//     clearGalleryMarkup();
+//     renderMarkupMovieCard(results);
+//     updateLocalStorage(results);
+//     findLi();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// function getQuery() {
+//   return searchForm.elements[0].value.trim();
+// }
+
+// pagination.on('afterMove', updatePagination);
+
+// async function updatePagination(event) {
+//   const currentPage = event.page;
+//   console.log('currentPage внизу', currentPage);
+
+//   const results = await fetchMovie(currentPage); //?????????????????
+//   clearGalleryMarkup();
+//   renderMarkupMovieCard(results);
+//   updateLocalStorage(results);
+//   findLi();
+// }
+
+// async function fetchMovie(page) {
+//   visibleSpinner();
+//   searchParams.set('page', page);
+
+//   const { data } = await axios.get(`${url}${searchParams}`);
+
+//   // для красивого спинера
+//   delay(500).then(() => {
+//     hideSpinner();
+//   });
+
+//   if (data.total_results === 0) {
+//     Notify.failure('Search result is not successful. Please, try again');
+//     searchForm.elements[0].value = '';
+//   }
+//   const { results, total_results } = data;
+//   pagination.reset(total_results);
+
+//   return results;
+// }
+// function clearGalleryMarkup() {
+//   gallery.innerHTML = '';
+// }
+// function updateLocalStorage(results) {
+//   localStorage.setItem('currentPopularMovies', JSON.stringify(results));
+// }
+
+// -----------------------------------------------------------------------------------
 
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // import renderMarkupMovieCard from './markapTempllate';
--- src/js/fetchPopularMovies.js
+++ src/js/fetchPopularMovies.js
@@ -2,50 +2,42 @@
 import renderMarkupMovieCard from './markapTempllate';
 import './openModal';
 import findLi from './openModal';
+
 import Pagination from 'tui-pagination';
+import FilmsPagination from './pagination-again';
 
 const axios = require('axios').default;
 
-const container = document.getElementById('pagination');
 const gallery = document.querySelector('.gallery');
-
 const API_KEY = '301d018a3b09052968e9ce18b1793bab';
 const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week?api_key=';
 
-const options = {
-  totalItems: 0,
-  itemsPerPage: 20,
-  visiblePages: 5,
-  page: 1,
-};
-
-const pagination = new Pagination(container, options);
-const pagePaginationNumber = pagination.getCurrentPage();
-console.log(pagePaginationNumber);
+let moviesDataforMarkupCreator;
+let paginator;
 
 export async function fetchPopularMovies(pagePaginationNumber = 1) {
   console.log(pagePaginationNumber);
   try {
+    console.log(`${BASE_URL}${API_KEY}&page=${pagePaginationNumber}`);
     const moviesDataArray = await axios.get(
       `${BASE_URL}${API_KEY}&page=${pagePaginationNumber}`
     );
-
     if (moviesDataArray.status != 200) {
       return;
     }
-
-    const moviesDataforMarkupCreator = moviesDataArray.data.results;
-    const totalResults = moviesDataArray.data.total_results;
-    pagination.reset(totalResults);
-
+    moviesDataforMarkupCreator = moviesDataArray.data.results;
+    //  console.log(moviesDataforMarkupCreator)
+    totalResults = moviesDataArray.data.total_results;
     localStorage.setItem(
       'currentPopularMovies',
       JSON.stringify(moviesDataforMarkupCreator)
     );
-
     clearGalleryMarkup();
+    paginator = new FilmsPagination(null, totalResults);
+    paginator.pagination.on('afterMove', paginatePage);
     renderMarkupMovieCard(moviesDataforMarkupCreator);
     findLi();
+    // return moviesDataforMarkupCreator;
   } catch (error) {
     console.log(error);
   }
@@ -55,11 +47,82 @@ function clearGalleryMarkup() {
   gallery.innerHTML = '';
 }
 
-pagination.on('afterMove', updatePage);
-
-async function updatePage(event) {
+// let responce;
+function paginatePage(event) {
   const currentPage = event.page;
-  console.log('currentPage', currentPage);
-
-  await fetchPopularMovies(currentPage);
+  //  console.log(event)
+  // paginator.pagination._currentPage = currentPage;
+  fetchPopularMovies(currentPage);
+  // responce = await fetchPopularMovies(currentPage);
+  // renderMarkupMovieCard(responce);
 }
+
+// --------------------------------------------------------------------------------------
+// внизу версия которая работала до пагинаци на классе
+// --------------------------------------------------------------------------------------
+
+// // https://api.themoviedb.org/3/trending/all/day?api_key='301d018a3b09052968e9ce18b1793bab'
+// import renderMarkupMovieCard from './markapTempllate';
+// import './openModal';
+// import findLi from './openModal';
+// import Pagination from 'tui-pagination';
+
+// const axios = require('axios').default;
+
+// const container = document.getElementById('pagination');
+// const gallery = document.querySelector('.gallery');
+
+// const API_KEY = '301d018a3b09052968e9ce18b1793bab';
+// const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week?api_key=';
+
+// const options = {
+//   totalItems: 0,
+//   itemsPerPage: 20,
+//   visiblePages: 5,
+//   page: 1,
+// };
+
+// const pagination = new Pagination(container, options);
+// const pagePaginationNumber = pagination.getCurrentPage();
+// console.log(pagePaginationNumber);
+
+// export async function fetchPopularMovies(pagePaginationNumber = 1) {
+//   console.log(pagePaginationNumber);
+//   try {
+//     const moviesDataArray = await axios.get(
+//       `${BASE_URL}${API_KEY}&page=${pagePaginationNumber}`
+//     );
+
+//     if (moviesDataArray.status != 200) {
+//       return;
+//     }
+
+//     const moviesDataforMarkupCreator = moviesDataArray.data.results;
+//     const totalResults = moviesDataArray.data.total_results;
+//     pagination.reset(totalResults);
+
+//     localStorage.setItem(
+//       'currentPopularMovies',
+//       JSON.stringify(moviesDataforMarkupCreator)
+//     );
+
+//     clearGalleryMarkup();
+//     renderMarkupMovieCard(moviesDataforMarkupCreator);
+//     findLi();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// function clearGalleryMarkup() {
+//   gallery.innerHTML = '';
+// }
+
+// pagination.on('afterMove', updatePage);
+
+// async function updatePage(event) {
+//   const currentPage = event.page;
+//   console.log('currentPage', currentPage);
+
+//   await fetchPopularMovies(currentPage);
+// }
